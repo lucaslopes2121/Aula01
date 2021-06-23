caaeafb55b401b6be3080a605da70c7c39b449f7
Lucas Araujo <araujo.lopes.souza@gmail.com>
2021-06-23 19:54:10 -0300

    Versão do final da aula 01

---
+++ src/LendoDados.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+
+public class LendoDados {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		String nome;
+		
+		System.out.println("Por favor, insira seu nome");
+		nome = leitor.nextLine();
+		System.out.println("Bem vindo, " + nome);
+		leitor.close();
+		
+		
+	}
+
+}
